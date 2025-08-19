c89a8a68f6ad52c8a3b5bcd9fed33cb1a09fd2b1
“Sathya <sathyayadav27@gmail.com>
2025-08-19 13:20:29 +0530

    put user details added one field

--- users/update_profile.php
+++ users/update_profile.php
@@ -14,7 +14,7 @@ $allowedFields = [
     'ProfileCreatedBy', 'MaritalStatus', 'gender', 'dob', 'Age', 'Height', 'AnyDisability', 'AboutMyself',
 
     // Family Details
-    'FatherOccupation', 'MotherOccupation', 'Brother', 'Sister', 'FamilyStatus', 'DietFood',
+    'FatherOccupation', 'MotherOccupation', 'Brother', 'Sister', 'FamilyStatus', 'DietFood', 'family details',
 
     // Religious Background
     'Religion', 'MotherTongue', 'Community', 'SubCast', 'CastNoBar', 'Gothram',
